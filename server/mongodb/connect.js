f5c1cb6d15d266a02aec90fe4607e9ddb50b9443
melvinUGO <ugochukwumelvin@gmail.com>
2023-01-27 11:51:37 -0800

    first commit

---
+++ server/mongodb/connect.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const connectDB = async (url) => {
+  mongoose.set("strictQuery", true);
+
+  mongoose
+    .connect(url)
+    .then(() => console.log("mongodb connected"))
+    .catch((error) => console.log(error));
+};
+
+export default connectDB;
